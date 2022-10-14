fa35f549f58db89477182206562f30f358e53eac
TM <gustavoplm@hotmail.com>
2022-10-14 20:41:28 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal:  (config, { configType }) => {
+    if (configType === 'Production') {
+      config.base = '/ignite-lab-design-system/'
+    }
+    return config
   }
+
 }
